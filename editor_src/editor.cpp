0876eb49d28732aa98de2544b17a65603b1b6fa5
Germán Rotili <german.rotili@gmail.com>
2020-12-10 18:39:42 -0300

    Agregado la posibilidad de introducir posiciones de inicio de pjs

--- editor_src/editor.cpp
+++ editor_src/editor.cpp
@@ -29,9 +29,13 @@ bool mapHasWall(int x, int y) {
   return (worldMap[y][x] == 1);
 }
 
-void toggleWall(int x, int y) {
-  if (mapHasWall(x, y)) worldMap[y][x] = 0;
-  else worldMap[y][x] = 1;
+int checkMap(int x, int y) {
+  if (x < 0 || x >= 10 || y < 0 || y >= 10) return 0;
+  return worldMap[y][x];
+}
+
+void toggleTile(int x, int y, int action) {
+  worldMap[y][x] = action;
 }
 
 int main(int argc, char* args[]) {
@@ -48,6 +52,8 @@ int main(int argc, char* args[]) {
     double actorX = 1;
     double actorY = 4;
 
+    int action = 0;
+
     //SdlTexture walls("textures/walls.png", renderer);
 
     bool quit = false;
@@ -58,12 +64,26 @@ int main(int argc, char* args[]) {
       while (SDL_PollEvent(&e) != 0) {
         if (e.type == SDL_QUIT) {
           quit = true;
+        } else if (e.type == SDL_KEYDOWN) {
+          switch (e.key.keysym.sym) {
+            case SDLK_w:
+            action = 1;
+            break;
+
+            case SDLK_q:
+            action = 2;
+            break;
+
+            case SDLK_e:
+            action = 0;
+            break;
+          }
         } else if (e.type == SDL_MOUSEBUTTONDOWN) {
           if (e.button.button == SDL_BUTTON_LEFT) {
             if (e.button.x > 128) {
               int x = int((e.button.x - 128) / 64);
               int y = int(e.button.y / 64);
-              toggleWall(x, y);
+              toggleTile(x, y, action);
             }
           }
         }
@@ -71,18 +91,22 @@ int main(int argc, char* args[]) {
 
       renderer.setRenderDrawColor(255, 255, 255, 255);
       renderer.renderClear();
+      int start = 0;
       // Dibujar mapa
       for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 10; j++) {
-          if (mapHasWall(i, j)) {
+          if (checkMap(i, j) == 1) {
             renderer.setRenderDrawColor(255, 100, 100, 255);
             renderer.renderFillRect((i*64) + 128, j*64, 64, 64);
           }
           renderer.setRenderDrawColor(0, 0, 0, 255);
           renderer.renderDrawRect((i*64) + 128, j*64, 64, 64);
+          if (checkMap(i, j) == 2) {
+            renderer.setRenderDrawColor(0, 255, 0, 255);
+            renderer.renderFillRect((i*64) + 156, (j*64) + 28, 8, 8);
+          }
         }
       }
-      renderer.renderFillRect((actorX*64) + 158, (actorY*64) + 30, 4, 4);
 
       SDL_Rect clip;
       clip.x = 0;

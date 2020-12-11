2f67e68e3a4636e4438eb432e4fef03af088f5e8
Germán Rotili <german.rotili@gmail.com>
2020-12-11 15:21:22 -0300

    Editor con scrolling y menu de opciones (basico)

--- editor_src/editor.cpp
+++ editor_src/editor.cpp
@@ -9,28 +9,27 @@
 
 #define SCREEN_WIDTH 768
 #define SCREEN_HEIGHT 640
+#define MAP_Y 12
+#define MAP_X 12
 
-int worldMap[10][10] =
+int worldMap[MAP_Y][MAP_X] =
                     {
-                      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-                      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                      {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
                     };
 
-bool mapHasWall(int x, int y) {
-  if (x < 0 || x >= 10 || y < 0 || y >= 10) return false;
-  return (worldMap[y][x] == 1);
-}
-
 int checkMap(int x, int y) {
-  if (x < 0 || x >= 10 || y < 0 || y >= 10) return 0;
+  if (x < 0 || x >= MAP_X || y < 0 || y >= MAP_Y) return 0;
   return worldMap[y][x];
 }
 
@@ -53,6 +52,8 @@ int main(int argc, char* args[]) {
     double actorY = 4;
 
     int action = 0;
+    int scrollY = 0;
+    int scrollX = 0;
 
     //SdlTexture walls("textures/walls.png", renderer);
 
@@ -66,16 +67,24 @@ int main(int argc, char* args[]) {
           quit = true;
         } else if (e.type == SDL_KEYDOWN) {
           switch (e.key.keysym.sym) {
-            case SDLK_w:
-            action = 1;
+            case SDLK_UP:
+            if (scrollY > 0)
+              scrollY -= 1;
             break;
 
-            case SDLK_q:
-            action = 2;
+            case SDLK_DOWN:
+            if (scrollY < MAP_Y - (SCREEN_HEIGHT/64))
+              scrollY += 1;
             break;
 
-            case SDLK_e:
-            action = 0;
+            case SDLK_LEFT:
+            if (scrollX > 0)
+              scrollX -= 1;
+            break;
+
+            case SDLK_RIGHT:
+            if (scrollX < MAP_X - ((SCREEN_WIDTH - 128)/64))
+              scrollX += 1;
             break;
           }
         } else if (e.type == SDL_MOUSEBUTTONDOWN) {
@@ -83,7 +92,23 @@ int main(int argc, char* args[]) {
             if (e.button.x > 128) {
               int x = int((e.button.x - 128) / 64);
               int y = int(e.button.y / 64);
-              toggleTile(x, y, action);
+              toggleTile(x+scrollX, y+scrollY, action);
+            } else if ((e.button.x >= 32 && e.button.x <= 96) &&
+              (e.button.y >= SCREEN_HEIGHT/5 &&
+              e.button.y <= (SCREEN_HEIGHT/5) + 64)) {
+              action = 0;
+            } else if ((e.button.x >= 32 && e.button.x <= 96) &&
+              (e.button.y >= 2 * (SCREEN_HEIGHT/5) &&
+              e.button.y <= (2 * (SCREEN_HEIGHT/5)) + 64)) {
+              action = 1;
+            } else if ((e.button.x >= 32 && e.button.x <= 96) &&
+              (e.button.y >= 3 * (SCREEN_HEIGHT/5) &&
+              e.button.y <= (3 * (SCREEN_HEIGHT/5)) + 64)) {
+              action = 2;
+            } else if ((e.button.x >= 32 && e.button.x <= 96) &&
+              (e.button.y >= 4 * (SCREEN_HEIGHT/5) &&
+              e.button.y <= (4 * (SCREEN_HEIGHT/5)) + 64)) {
+              action = 3;
             }
           }
         }
@@ -93,20 +118,40 @@ int main(int argc, char* args[]) {
       renderer.renderClear();
       int start = 0;
       // Dibujar mapa
-      for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+      for (int i = scrollX; i < MAP_X; i++) {
+        for (int j = scrollY; j < MAP_Y; j++) {
           if (checkMap(i, j) == 1) {
             renderer.setRenderDrawColor(255, 100, 100, 255);
-            renderer.renderFillRect((i*64) + 128, j*64, 64, 64);
+            renderer.renderFillRect(((i-scrollX)*64) + 128, (j-scrollY)*64, 64, 64);
           }
           renderer.setRenderDrawColor(0, 0, 0, 255);
-          renderer.renderDrawRect((i*64) + 128, j*64, 64, 64);
+          renderer.renderDrawRect(((i-scrollX)*64) + 128, (j-scrollY)*64, 64, 64);
           if (checkMap(i, j) == 2) {
             renderer.setRenderDrawColor(0, 255, 0, 255);
-            renderer.renderFillRect((i*64) + 156, (j*64) + 28, 8, 8);
+            renderer.renderFillRect(((i-scrollX)*64) + 156, ((j-scrollY)*64) + 28, 8, 8);
+          } else if (checkMap(i, j) == 3) {
+            renderer.setRenderDrawColor(205, 133, 63, 255);
+            renderer.renderFillRect(((i-scrollX)*64) + 128, ((j-scrollY)*64) + 30, 64, 4);
           }
         }
       }
+
+      renderer.setRenderDrawColor(100, 100, 100, 255);
+      renderer.renderFillRect(0, 0, 128, SCREEN_HEIGHT);
+
+      renderer.setRenderDrawColor(255, 255, 255, 255);
+      renderer.renderFillRect(32, SCREEN_HEIGHT/5, 64, 64);
+
+      renderer.setRenderDrawColor(255, 100, 100, 255);
+      renderer.renderFillRect(32, 2 * (SCREEN_HEIGHT/5), 64, 64);
+
+      renderer.setRenderDrawColor(0, 255, 0, 255);
+      renderer.renderFillRect(62, (3 * (SCREEN_HEIGHT/5)) + 30, 8, 8);
+
+      renderer.setRenderDrawColor(205, 133, 63, 255);
+      renderer.renderFillRect(32, 4 * (SCREEN_HEIGHT/5), 64, 4);
+
+
 
       SDL_Rect clip;
       clip.x = 0;

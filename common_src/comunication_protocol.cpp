3c5e4928e91e5b38c2aa327aeb7be0e61023a23b
Andres Tomas Visciglio <atvisciglio@gmail.com>
2021-01-18 20:31:00 -0300

    error

--- common_src/comunication_protocol.cpp
+++ common_src/comunication_protocol.cpp
@@ -20,7 +20,7 @@ void Snapshot::update_player(){
     this->player += "position:{x:1,y:1,o:90},weapons:pistol,life:100,attack:false/";
 }
 
-const char* Snapshot::get_snapshot(){ 
+const char* Snapshot::get_snapshot(){
 
     std::string buffer = std::to_string(this->object.size());
     buffer += object;

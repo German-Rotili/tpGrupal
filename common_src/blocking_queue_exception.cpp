0c3c1424587a8f6f2a822b1335255bed6352cb5c
Andres Tomas Visciglio <atvisciglio@gmail.com>
2021-02-09 18:33:13 -0300

    clean

--- common_src/blocking_queue_exception.cpp
+++
@@ -1,5 +0,0 @@
-#include "blocking_queue_exception.h"
-
-const char* BlockingQueueGetException::what(){
-  return "No elements in queue.";
-}
--- server_src/Map.cpp
+++ server_src/Map.cpp
@@ -131,7 +131,6 @@ void Map::populate_variables() {
   }
 }
 
-
 void Map::execute_intentions(std::vector<char> & intentions, int & client_id){
   for (Player &player : this->players) {
     if(player.get_id() == client_id){

acdf9ab7feed1bdfe30d7a4e4313171f5b4a3456
Luciano <lucianortiz97@gmail.com>
2021-02-09 21:51:39 -0300

    Saco cosas viejas del client.cpp y ajusto config

--- client_src/client.cpp
+++ client_src/client.cpp
@@ -6,9 +6,6 @@
 #include "../common_src/SDLWrappers/SdlContexto.h"
 #include "../common_src/SDLWrappers/SdlWindow.h"
 #include "../common_src/SDLWrappers/SdlRenderer.h"
-#include "../common_src/SDLWrappers/SdlException.h"
-#include "../common_src/SDLWrappers/SdlTexture.h"
-#include "../common_src/SDLWrappers/SdlFont.h"
 #include "World.h"
 #include "ClientSettings.h"
 #include "client_helper.h"
@@ -18,25 +15,21 @@
 #include "Menu.h"
 #include "ClientConfigHandler.h"
 #include "../common_src/Action.h"
-#include <map>
 
 #define FPS 30
 #define HOSTNAME 1
 #define SERVICE 2
 int main(int argc, char* args[]) {
   try {
-    const char* WINDOW_NAME = "Wolfenstein Client";
-    const bool FULLSCREEN = false;
     std::string hostname = args[HOSTNAME];
     std::string service = args[SERVICE];
     Client client(service, hostname);
-
 
     ClientConfigHandler config;
     config.initConfig("../resources/config/clientConfig.yaml");
     ClientSettings settings(config.getWidth(), config.getHeight(), FPS, config.getFOV());
     SdlContexto contexto;  // Inicializa SDL, image, ttf y mixer
-
+    const char* WINDOW_NAME = "Wolfenstein Client";
     SdlWindow window(WINDOW_NAME, settings.screenWidth, settings.screenHeight, config.getFullscreen());
 
     SdlRenderer renderer = window.getRenderer();
@@ -52,8 +45,6 @@ int main(int argc, char* args[]) {
     bool quit = false;
     SDL_Event e;
     const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
-
-
 
     /*Creamos y corremos los hilos*/
     std::vector<char> intention;
@@ -102,26 +93,23 @@ int main(int argc, char* args[]) {
         }
       }
       if (currentKeyStates[SDL_SCANCODE_W]) {
-            intention.push_back(SDL_SCANCODE_W);
+        intention.push_back(SDL_SCANCODE_W);
       }
       if (currentKeyStates[SDL_SCANCODE_S]) {
-            intention.push_back(SDL_SCANCODE_S);
+        intention.push_back(SDL_SCANCODE_S);
       }
       if (currentKeyStates[SDL_SCANCODE_A ]) {
-            intention.push_back(SDL_SCANCODE_A);
+        intention.push_back(SDL_SCANCODE_A);
       }
       if (currentKeyStates[SDL_SCANCODE_D ]) {
-            intention.push_back(SDL_SCANCODE_D);
-
+        intention.push_back(SDL_SCANCODE_D);
       }
       if (currentKeyStates[SDL_SCANCODE_SPACE ]) {
-            intention.push_back(SDL_SCANCODE_SPACE);
+        intention.push_back(SDL_SCANCODE_SPACE);
       }
 
       update_snapshot = requester.get_snapshot();
-
       world.actualizar(update_snapshot);
-
       world.renderizar(settings);
 
       std::chrono::steady_clock::time_point end_time = std::chrono::steady_clock::now();
@@ -144,7 +132,7 @@ int main(int argc, char* args[]) {
     std::cout << e.what();
   }
   catch (...) {
-
+    printf("Error inesperado\n");
   }
   return 0;
 }

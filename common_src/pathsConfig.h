237a8576f1187bfd7ec877c3411e029800d4075f
Germán Rotili <29235133+German-Rotili@users.noreply.github.com>
2021-02-20 19:42:12 -0300

    Update path MapList

--- common_src/pathsConfig.h
+++ common_src/pathsConfig.h
@@ -7,7 +7,7 @@
 #define CLIENT_CONFIG_PATH "../resources/config/clientConfig.yaml"
 #define SERVER_CONFIG_PATH "../resources/config/serverConfig.yaml"
 #define EDITOR_CONFIG_PATH "../resources/config/editorConfig.yaml"
-#define MAP_LIST_PATH "../resources/config/MAPLIST.yaml"
+#define MAP_LIST_PATH "../resources/config/MapList.yaml"
 
 #else
 
@@ -15,7 +15,7 @@
 #define CLIENT_CONFIG_PATH "var/wolfenstein/config/clientConfig.yaml"
 #define SERVER_CONFIG_PATH "var/wolfenstein/config/serverConfig.yaml"
 #define EDITOR_CONFIG_PATH "var/wolfenstein/config/editorConfig.yaml"
-#define MAP_LIST_PATH "var/wolfenstein/config/MAPLIST.yaml"
+#define MAP_LIST_PATH "var/wolfenstein/config/MapList.yaml"
 
 #endif
 
